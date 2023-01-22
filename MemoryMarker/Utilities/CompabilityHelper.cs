f596e72af4e933a43ce4b7aa7973604c611d1ed5
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-01-21 21:28:37 -0800

    Change WaymarkPreset warning phrasing

--- MemoryMarker/Utilities/CompabilityHelper.cs
+++ MemoryMarker/Utilities/CompabilityHelper.cs
@@ -5,5 +5,5 @@ namespace MemoryMarker.Utilities;
 public static class CompabilityHelper
 {
     public static bool IsWaymarkPresetInstalled() => Service.PluginInterface.PluginInternalNames.Any(internalName => internalName == "WaymarkPresetPlugin");
-    public const string WaymarkPresetWarning = "WaymarkPreset plugin detected, to prevent conflicts MemoryMarker has been disabled.";
+    public const string WaymarkPresetWarning = "WaymarkPreset plugin detected, skipping writing waymarks to memory.";
 }
